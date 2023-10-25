b8995026b5d7a38ce6b52731a31cc650da32ceb4
sukerberk1 <szymon.l3232@gmail.com>
2023-10-25 21:54:45 +0200

    Add authorization, add checking and deleting tasks

--- atlas_manager/tasks/urls.py
+++ atlas_manager/tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TasksTemplateView, TaskDetailView
+from .views import TasksTemplateView, TaskDetailView, TaskDeleteView, TaskCheckView
 
 urlpatterns = [
     path("template/", TasksTemplateView.as_view(), name="tasks"),
-    path("<int:pk>", TaskDetailView.as_view(), name="task_detail")
+    path("<int:pk>", TaskDetailView.as_view(), name="task_detail"),
+    path("<int:pk>/delete", TaskDeleteView.as_view(), name="task_delete"),
+    path("<int:pk>/check", TaskCheckView.as_view(), name="task_check"),
 ]
--- atlas_manager/tasks/views.py
+++ atlas_manager/tasks/views.py
@@ -1,11 +1,15 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, DetailView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from projects.models import Project
 from tasks.models import Task
 
 # Create your views here.
 
-class TasksTemplateView(TemplateView):
+class TasksTemplateView(LoginRequiredMixin, TemplateView):
     template_name = "tasks.html"
 
     def get_context_data(self, **kwargs):
@@ -18,8 +22,49 @@ class TasksTemplateView(TemplateView):
         context["project_tasks"] = project_tasks
         return context
 
-class TaskDetailView(DetailView):
+class TaskDetailView(UserPassesTestMixin, DetailView):
     model = Task
     template_name = "task_detail.html"
     context_object_name = "task"
+
+    def test_func(self):
+        task = self.get_object()
+        return task.project.author == self.request.user
     
+
+class TaskDeleteView(UserPassesTestMixin, DeleteView):
+    model = Task
+    template_name = "task_delete.html"
+    success_url = reverse_lazy("tasks")
+
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        self.object = self.get_object()
+
+        if self.object.prev:
+            prev_task = self.object.prev
+            next_task = self.object.next
+            self.object.delete()
+
+            next_task.prev = prev_task
+            next_task.save()
+        else:
+            self.object.delete()
+        
+        return HttpResponseRedirect(self.get_success_url())
+    
+    def test_func(self):
+        task = self.get_object()
+        return task.project.author == self.request.user
+
+
+class TaskCheckView(UpdateView):
+    model = Task
+    template_name = "task_check.html"
+    success_url = reverse_lazy("tasks")
+    fields=[]
+
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        self.object = self.get_object()
+        self.object.done = True
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
